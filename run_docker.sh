1530919bf71a36d8510ae7e1ccc39fb26db7893d
default_username <default_email@example.com>
2026-03-10 02:34:34 -0400

    Fixing leakage and refactoring

--- run_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Costruisci l'immagine Docker
-docker build -t agone_test_image .
-
-# Esegui il container Docker, montando la directory corrente
-docker run --rm -v "$(pwd):/app" agone_test_image
